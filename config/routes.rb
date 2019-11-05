f7e6b031cb93409b55fe35db1b09422e81c33d81
Yoni <y_benatar@hotmail.com>
2019-11-06 00:42:20 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   get 'static_pages/secret'
 	resources :event
-  root to: 'static_pages#index'
-  #root to: 'events#index' # page d'accueil
+  #root to: 'static_pages#index'
+  root to: 'events#index' # page d'accueil
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
